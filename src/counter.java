aadd95dab755d468f530cd460070bfa4004a8f2e
boostq <haopeng.li@office-365.work>
2022-01-15 12:52:24 +0800

    1.2 数据抽象 Counter 实现

---
+++ src/counter.java
@@ -0,0 +1,33 @@
+import static edu.princeton.cs.algs4.StdOut.printf;
+
+public  class counter {
+    private final String name;
+    private int count = 0;
+    public counter(String id){
+        this.name=id;
+    }
+    public void increment(){
+        this.count++;
+    }
+    public int tally(){
+        return count;
+    }
+    public String get_id(){
+        return this.name;
+    }
+@Override
+public String toString(){
+        return ""+count+name;
+}
+//测试模块
+    public static void main(String[] args) {
+    counter n =new counter("tes");
+    printf("%d\n",n.tally());
+    n.increment();
+    printf("%d\n",n.tally());
+    printf(n.get_id()+"\n");
+// toString 从 Object 中继承
+        printf(n.toString());
+    }
+}
+
