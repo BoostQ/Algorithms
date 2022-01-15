59f9f22b6c3c905ecd2206c2f3031503c5550399
boostq <haopeng.li@office-365.work>
2022-01-15 11:39:05 +0800

    first commit

---
+++ src/test.java
@@ -0,0 +1,19 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+import static algorithms.search.BinarySearch.rank;
+
+public class test {
+    public static void main(String[] args) {
+        int[] whitelist = In.readInts(args[0]);
+        Arrays.sort(whitelist);
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (rank(key, whitelist) < 0)
+                StdOut.println(key);
+        }
+    }
+}
